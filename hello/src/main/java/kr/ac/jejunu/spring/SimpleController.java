29eaf17e90125984ea69675fe17fffe2b294131d
Wef2 <qhrud5725@hanmail.net>
2016-05-13 17:03:08 +0900

    Simple Hello World

---
+++ hello/src/main/java/kr/ac/jejunu/spring/SimpleController.java
@@ -0,0 +1,21 @@
+package kr.ac.jejunu.spring;
+
+/**
+ * Created by Kim on 2016-05-13.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("/simple")
+public class SimpleController implements org.springframework.web.servlet.mvc.Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView("simple");
+        modelAndView.addObject("abc", "Hello, World!");
+        return modelAndView;
+    }
+}
--- hello/src/main/webapp/WEB-INF/views/simple.jsp
+++ hello/src/main/webapp/WEB-INF/views/simple.jsp
@@ -1,10 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Hello Test</title>
+    <title>Simple</title>
 </head>
 <body>
-    <h1>hello test</h1>
-
+    <h1>${abc}</h1>
 </body>
 </html>
